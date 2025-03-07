8765382916adad1e6523c04b39603019b0bce4dc
Kostya <baldakov.kostya@yandex.ru>
2025-03-07 18:46:28 +0300

    i added pagination and eliminated prop drilling by introducing context

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 import { Route, Routes } from "react-router-dom";
+import { useState, createContext } from "react";
 import Header from "./components/Header";
 import NotFound from "./components/NotFound";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import "./scss/app.scss";
 
+export const SearchContext = createContext();
+
 function App() {
+  const [searchValue, setSearchValue] = useState("");
   return (
     <div className="wrapper">
-      <Header />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
+      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home searchValue={searchValue} />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </SearchContext.Provider>
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,22 +3,25 @@ import Categories from "../components/Categories";
 import PizzaBlock from "../components/PizzaBlock";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 import Sort from "../components/Sort";
+import Pagination from "../components/Pagination";
 
-const Home = () => {
+const Home = ({ searchValue }) => {
   const [items, setItems] = useState([]);
   const [isLoader, setIsLoader] = useState(true);
   const [categoryId, setCategoryId] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [sortType, setSortType] = useState({
     name: "популярности",
     sort: "rating",
   });
 
   const allCategory = categoryId > 0 ? `category=${categoryId}` : "";
+  const search = searchValue ? `&search=${searchValue}` : "";
 
   useEffect(() => {
     setIsLoader(true);
     fetch(
-      `https://67b4aaf3a9acbdb38ecfeebc.mockapi.io/items?${allCategory}&sortBy=${sortType.sort}&order=desc`
+      `https://67b4aaf3a9acbdb38ecfeebc.mockapi.io/items?page=${currentPage}&limit=4&${allCategory}&sortBy=${sortType.sort}&order=desc${search}`
     )
       .then((res) => res.json())
       .then((res) => {
@@ -26,7 +29,13 @@ const Home = () => {
         setIsLoader(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue, currentPage]);
+
+  const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+
+  const skeletons = [...new Array(6)].map((_, index) => (
+    <Skeleton key={index} />
+  ));
 
   return (
     <div className="container">
@@ -38,11 +47,10 @@ const Home = () => {
         <Sort sortType={sortType} onChangeSort={(i) => setSortType(i)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoader
-          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-      </div>
+      <div className="content__items">{isLoader ? skeletons : pizzas}</div>
+      <Pagination
+        onChangePage={(number: number) => setCurrentPage(number)}
+      />
     </div>
   );
 };
